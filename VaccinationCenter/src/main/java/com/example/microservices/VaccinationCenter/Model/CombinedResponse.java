fea5f9b25857175597b97f236d99e6a77cda0c80
shubhambadhe2114 <84230765+shubhambadhe2114@users.noreply.github.com>
2022-05-30 21:12:07 +0530

    correction

--- VaccinationCenter/src/main/java/com/example/microservices/VaccinationCenter/Model/CombinedResponse.java
+++ VaccinationCenter/src/main/java/com/example/microservices/VaccinationCenter/Model/CombinedResponse.java
@@ -19,16 +19,4 @@ public class CombinedResponse {
 
 	private VaccinationCenter center;
 	private List<Citizen> citizens;
-	public VaccinationCenter getCenter() {
-		return center;
-	}
-	public void setCenter(VaccinationCenter center) {
-		this.center = center;
-	}
-	public List<Citizen> getCitizens() {
-		return citizens;
-	}
-	public void setCitizens(List<Citizen> citizens) {
-		this.citizens = citizens;
-	}
 }
